1805b38b5b86187cc7a31bd6c2ea83a37fe813e5
Eric Fries <ericfries@Erics-MacBook-Pro-2.local>
2018-10-07 18:37:04 -0400

    add basic express server and heroku commands

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname + '/dist'));
+
+// send the user to index html page regarless of the url
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+});
+
+app.listen(8080);

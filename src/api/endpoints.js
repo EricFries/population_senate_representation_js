4e306daf49c91a2cd1b743fce6aff2e60a1e3f10
Eric Fries <ericfries@Erics-MacBook-Pro-2.local>
2018-10-07 18:38:07 -0400

    renable production url

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,6 +1,6 @@
-// const BASE_ENDPOINT = 'https://powerful-waters-68246.herokuapp.com';
+const BASE_ENDPOINT = 'https://powerful-waters-68246.herokuapp.com';
 
 // Use for local development with Django dev server.
-const BASE_ENDPOINT = 'http://127.0.0.1:8000';
+// const BASE_ENDPOINT = 'http://127.0.0.1:8000';
 export const HISTORICAL_POPULATIONS_MAX_MIN_ENDPOINT = `${BASE_ENDPOINT}/historical_populations/max_min_groups/`;
 export const STATES_ENDPOINT = `${BASE_ENDPOINT}/states/`;

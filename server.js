261bbefef27959ed9002928cd61e8a90e718874f
Eric Fries <ericfries@Erics-MacBook-Pro-2.local>
2018-10-07 18:48:29 -0400

    adjust express port for express

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 8080;
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname + '/dist'));
@@ -10,4 +11,4 @@ app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
-app.listen(8080);
+app.listen(port);

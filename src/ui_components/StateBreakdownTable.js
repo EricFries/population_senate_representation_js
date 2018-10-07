2aca3effc407bc4b45bb34be84e265d4a4db9ec4
Eric Fries <ericfries@Erics-MacBook-Pro-2.local>
2018-10-07 19:10:51 -0400

    add table margin on small screens

--- src/ui_components/StateBreakdownTable.js
+++ src/ui_components/StateBreakdownTable.js
@@ -6,10 +6,14 @@ const StateBreakdownTable = styled.table`
 	@media (max-width: 768px) {
 		width: 100%;
 		padding: 0;
+		margin-top: 1rem;
+		:first-of-type {
+			margin-top: 0;
+		}
 	}
 	width: 50%;
 	padding: 1rem;
-	box-izing: border-box;
+	box-sizing: border-box;
 `;
 
 export default StateBreakdownTable;
